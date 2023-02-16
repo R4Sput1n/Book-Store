4c2a1aaff5f548bbf0c9047aa9e8ac1ea200383d
Kacper Piątek <kacperpiatek01@gmail.com>
2023-02-16 21:30:05 +0100

    Add new UserAuthor model and migrate

--- accounts/models.py
+++ accounts/models.py
@@ -1,32 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Books
+from store.models import Authors
 
-# Create your models here.
 
-#
-# class Accounts(models.Model):
-#     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-#     account_id = models.UUIDField(primary_key=True)
-#     login = models.CharField(max_length=50)
-#     email = models.CharField(max_length=100)
-#     salt = models.UUIDField()
-#     passhash = models.TextField()
-#
-#     def __str__(self):
-#         return self.login
-#
-#     class Meta:
-#         managed = False
-#         db_table = 'accounts'
-#         unique_together = (('login', 'email'),)
+class UserAuthor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_author = models.BooleanField(default=False)
+    author_ref = models.ForeignKey(Authors, on_delete=models.DO_NOTHING, blank=True, null=True)
 
 
 class OwnedProducts(models.Model):
     owner = models.ForeignKey(User, models.DO_NOTHING, blank=True, null=True)
     product = models.ForeignKey(Books, models.DO_NOTHING, blank=True, null=True)
-
-    # class Meta:
-    #     managed = False
-    #     db_table = 'owned_products'
 
