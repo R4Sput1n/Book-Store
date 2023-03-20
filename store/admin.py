8029fa313d8050eeff5b90679bd41ed2154af935
Kacper Piątek <kacperpiatek01@gmail.com>
2023-03-20 21:06:01 +0100

    Add filter and display fields to Books admin

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Books, Authors, Series
 
-admin.site.register(Books)
+
+@admin.register(Books)
+class BooksAdmin(admin.ModelAdmin):
+    list_display = ['book_title', 'author', 'series', 'volume_number', 'genre', 'price']
+    list_filter = ['genre']
+
+
 admin.site.register(Authors)
 admin.site.register(Series)
