22b21447087a900710401398c9b3f20e03f3bf96
Kacper Piątek <kacperpiatek01@gmail.com>
2023-03-22 18:10:13 +0100

    Add pagination to homepage

--- store/views.py
+++ store/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Books
 
 
 def home(request):
+    book_list = Books.objects.all()
+    paginator = Paginator(book_list, 8)
+    page_num = request.GET.get('page', 1)
+
+    try:
+        books = paginator.page(page_num)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+
     context = {
-        'books': Books.objects.all(),
+        'books': books,
         'book_genres': Books.objects.all().order_by('genre').distinct('genre')  # Books are sorted on default
         # by book_title (.models line 35). Distinct needs ordering by itself
     }
@@ -13,8 +25,19 @@ def home(request):
 
 
 def genre(request, genre):
+    book_list = Books.objects.filter(genre=genre)
+    paginator = Paginator(book_list, 8)
+    page_num = request.GET.get('page', 1)
+
+    try:
+        books = paginator.page(page_num)
+    except PageNotAnInteger:
+        books = paginator.page(1)
+    except EmptyPage:
+        books = paginator.page(paginator.num_pages)
+
     context = {
-        'books': Books.objects.filter(genre=genre),
+        'books': books,
         'book_genres': Books.objects.all().order_by('genre').distinct('genre')  # comment - line 9
     }
     return render(request, 'store/home.html', context)
